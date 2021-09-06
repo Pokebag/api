531dbbc2451081304318e1c1f43d91d42cebf8e4
Trezy <t@trezy.com>
2021-09-06 00:02:10 -0500

    refactor(routes/pokemon): update for JSON:API compliance

--- src/routes/unite/pokemon.js
+++ src/routes/unite/pokemon.js
@@ -1,5 +1,8 @@
 // Module imports
-import { getPokemon } from '@pokebag/data-sdk'
+import {
+	getPokemon,
+	getSkills,
+} from '@pokebag/data-sdk'
 
 
 
@@ -24,16 +27,85 @@ export class PokemonRoute extends Route {
 	 */
 	async handler (context) {
 		try {
-			const POKEMON = await getPokemon({
-				includeSkills: true,
-				patch: context.params.patchVersion,
+			let includes = context.query.include
+			let pokemon = null
+			let skills = null
+
+			if (!Array.isArray(includes)) {
+				includes = [includes]
+			}
+
+			pokemon = await getPokemon({ patch: context.params.patchVersion })
+
+			if (includes.includes('skills')) {
+				skills = await getSkills({ patch: context.params.patchVersion })
+			}
+
+			context.data = pokemon.map(mon => {
+				const attributes = { ...mon }
+
+				delete attributes.id
+
+				const relationships = {
+					skills: {
+						links: {
+							related: `/unite/skills?filter[pokemon]=${mon.id}`,
+						},
+					},
+				}
+
+				if (skills) {
+					relationships.skills.data = skills.reduce((accumulator, skill) => {
+						if (skill.id.startsWith(mon.id)) {
+							accumulator.push({
+								id: skill.id,
+								type: 'skill',
+							})
+						}
+
+						return accumulator
+					}, [])
+				}
+
+				return {
+					attributes,
+					id: mon.id,
+					relationships,
+					type: 'pokemon',
+				}
 			})
 
-			context.data = {
-				pokemon: POKEMON.reduce((accumulator, mon) => {
-					accumulator[mon.id] = mon
-					return accumulator
-				}, {}),
+			if (skills) {
+				// @ts-ignore
+				context.included = skills.map(skill => {
+					const attributes = { ...skill }
+
+					delete attributes.id
+
+					const OWNER = pokemon.find(mon => {
+						return skill.id.startsWith(mon.id)
+					})
+
+					return {
+						attributes,
+						id: skill.id,
+						links: {
+							self: `/unite/skills/${skill.id}`,
+						},
+						relationships: {
+							pokemon: {
+								data: {
+									id: OWNER.id,
+									type: 'pokemon',
+								},
+								links: {
+									self: `/unite/pokemon/${OWNER.id}`,
+								},
+							},
+						},
+						type: 'skill',
+					}
+				})
 			}
 		} catch (error) {
 			context.errors.push(error.message)
