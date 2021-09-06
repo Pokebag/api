c62bfdd58c2bc014bad0ed4e5c2e5a742ed5d710
Trezy <t@trezy.com>
2021-09-06 00:03:08 -0500

    refactor(routes/unite): update for JSON:API compliance

--- src/routes/unite/unite.js
+++ src/routes/unite/unite.js
@@ -27,13 +27,31 @@ export class UniteRoute extends Route {
 			const PATCHES = await getPatches()
 
 			context.data = {
-				availablePatches: PATCHES.filter((patchVersion) => {
-					return !patchVersion.startsWith('200.')
-				}),
-				description: 'Join Trainers from around the world as they head for Aeos Island to compete in Unite Battles! In Unite Battles, Trainers face off in 5–on–5 team '
-				+ 'battles to see who can score the most points within the allotted time. Teamwork is key as you and your teammates defeat wild Pokémon, level up and evolve your '
-				+ 'partner Pokémon, and knock out the opposing team\'s Pokémon to prevent them from scoring points. Put your teamwork to the test, and take home the win!',
-				name: 'Pokémon UNITE',
+				id: 'unite',
+				type: 'game',
+				attributes: {
+					availablePatches: PATCHES.filter((patchVersion) => {
+						return !patchVersion.startsWith('200.')
+					}),
+					description: 'Join Trainers from around the world as they head for Aeos Island to compete in Unite Battles! In Unite Battles, Trainers face off in 5–on–5 team '
+						+ 'battles to see who can score the most points within the allotted time. Teamwork is key as you and your teammates defeat wild Pokémon, level up and '
+						+ 'evolve your partner Pokémon, and knock out the opposing team\'s Pokémon to prevent them from scoring points. Put your teamwork to the test, and take '
+						+ 'home the win!',
+					displayName: 'Pokémon UNITE',
+				},
+				relationships: {
+					heldItems: {
+						links: {
+							related: '/unite/held-items',
+						},
+					},
+
+					pokemon: {
+						links: {
+							related: '/unite/pokemon',
+						},
+					},
+				},
 			}
 		} catch (error) {
 			context.errors.push(error.message)
